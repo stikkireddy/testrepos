79ffe7b8d816f94df7ac505f4a10c7c221b937e6
stikkireddy <54602805+stikkireddy@users.noreply.github.com>
2021-07-19 17:24:54 -0400

    Create main.py

---
+++ custom_functions/main.py
@@ -0,0 +1,7 @@
+import custom_functions.funcs as F
+
+def main():
+  print("running magic function")
+  value = F.my_magic_function(1, 2)
+  print(f"calculated value = {value}")
+  print("finished job")
