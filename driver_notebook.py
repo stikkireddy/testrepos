4130cfb676446ca5f731a0bfef0b399b4dfe7275
stikkireddy <54602805+stikkireddy@users.noreply.github.com>
2021-08-05 18:10:06 -0400

    done (#1)

--- driver_notebook.py
+++ driver_notebook.py
@@ -1,7 +1,29 @@
 # Databricks notebook source
+
+
+# COMMAND ----------
+
+python git clone
+python setup bdist wheel
+push to s3
+
+# COMMAND ----------
+
+# DBTITLE 1,Notebooks in repo get updated in Prod, Job config in prod get updated
+url + token
+fetch-pull repos api code
+push job config to be latest
+
+# COMMAND ----------
+
 from custom_functions.main import main
 
 main()
+
+# COMMAND ----------
+
+with open("./custom_functions/funcs.py", "r") as f:
+  data = f.read()
 
 # COMMAND ----------
 
